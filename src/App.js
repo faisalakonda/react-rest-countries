ff67c97688dfc470d8be90b9d843ec7395913bef
faisalakonda <faisalakonda2@gmail.com>
2023-05-08 09:48:18 +0600

    All countries show

--- src/App.js
+++ src/App.js
@@ -20,21 +20,30 @@ function LoadCountries(){
     <div>
       <h1>Visiting every country of the world</h1>
       <h3>Available countries: {countries.length}</h3>
-      {
-        countries.map(country => <Country name={country.name.common} continents={country.continents} capital={country.capital}></Country>)
-      }
+      <nav className='manu'>
+      <a href="/home">Home</a>
+    <a href="/shop">Shop</a>
+    <a href="/about">About</a>
+      </nav>
+      <div className='country-container'>
+        {
+          countries.map(country => <Country country={country}key={country.cca3}></Country>)
+        }
+      </div>
     </div>
   )
 }
 
 function Country(props){
+  const {continents, name, capital, population, timezones, flags} = props.country
   return(
     <div className='container'>
-      <h2>Continents: {props.continents
-}
-</h2>
-      <h3>Country: {props.name}</h3>
-     <h4>Capital: {props.capital}</h4> 
+      <h2>Continents: {continents}</h2>
+      <img src={flags.png} alt="" />
+      <h3>Country: {name.common}</h3>
+     <h4>Capital: {capital}</h4>
+     <h5>Populations: {population}</h5>
+     <h6>Time Zone: {timezones}</h6> 
     </div>
   )
 }
